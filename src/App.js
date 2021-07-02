8147eedbf907fb2e5c29c5fc719a47f36a84c474
Mario Schuetz <mayo2@gmx.de>
2021-07-02 14:15:25 +0200

    Retrieve changes from search bar

--- src/App.js
+++ src/App.js
@@ -5,17 +5,39 @@ import SearchBar from './components/layout/SearchBar';
 import ChargeMap from './components/map/ChargeMap';
 
 class App extends Component {
-  
+
   state = {
+    connectorTypes: new Set(["CCS", "CHAdeMO", "Type 2"]),
     currentPosition: [52.5208, 13.4094],
+    distance: 100,
+  }
+
+  componentDidMount() {
+    this.updateCurrentPosition();
+  }
+
+  updateCurrentPosition = () => {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      console.log("Latitude is :", position.coords.latitude);
+      console.log("Longitude is :", position.coords.longitude);
+      // this.setState({currentPosition: [position.coords.latitude, position.coords.longitude]});
+    });
+  }
+
+  handleChangeDistance = (distance) => {
+    this.setState({ distance });
+  }
+
+  handleChangeConnectorTypes = (connectorTypes) => {
+    this.setState({ connectorTypes });
   }
 
   render() {
     return (
       <div className="App">
         <Navbar />
-        <SearchBar />
-        <ChargeMap currentPosition={this.state.currentPosition}/>
+        <SearchBar distance={this.state.distance} onChangeDistance={this.handleChangeDistance} connectorTypes={this.state.connectorTypes} onChangeConnectorTypes={this.handleChangeConnectorTypes} />
+        <ChargeMap currentPosition={this.state.currentPosition} />
       </div>
     );
   }
--- src/components/layout/SearchBar.js
+++ src/components/layout/SearchBar.js
@@ -1,61 +1,71 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+
+
+  connectorTypeOptions = ["CCS", "CHAdeMO", "Type 2"];
+
   state = {
-    distance: 100,
     distanceError: '',
-    connectorTypes: new Set(["CCS", "CHAdeMO", "Type 2"]),
   }
 
-  handleChange = (e) => {
-    this.setState({
-      [e.target.id]: e.target.value
-    });
+  handleChangeDistance = (e) => {
+    if (this.validate(e.target.value)) {
+      this.props.onChangeDistance(e.target.value);
+    }
+  }
+
+  validate = (distance) => {
+    let distanceError = '';
+
+    if (!distance || distance <= 0) {
+      distanceError = 'Invalid distance';
+      this.setState({ distanceError });
+      return false;
+    }
+
+    this.setState({ distanceError });
+    return true
   }
 
   toggleConnectorTypes = (e) => {
     let ctypeValue = e.target.value;
-    if (this.state.connectorTypes.has(ctypeValue)) this.state.connectorTypes.delete(ctypeValue);
-    else this.state.connectorTypes.add(ctypeValue);
+    let connectorTypes = this.props.connectorTypes;
+    if (connectorTypes.has(ctypeValue)) connectorTypes.delete(ctypeValue);
+    else connectorTypes.add(ctypeValue);
+
+    this.props.onChangeConnectorTypes(connectorTypes);
   }
 
   render() {
+    const connectorTypes = this.props.connectorTypes;
+
     return (
       <div className="wrapper grey darken-2">
         <div className="row">
           <div className="col s6 m3">
             <div className="input-field">
               <label htmlFor="distance">Distance in Km</label>
-              <input className="white-text align-right" type="text" id="distance" value={this.state.distance} onChange={this.handleChange} />
+              <input className="white-text align-right" type="text" id="distance" value={this.props.distance} onChange={this.handleChangeDistance} />
               {this.state.distanceError ? (
                 <div className="err_msg">{this.state.distanceError}</div>
               ) : null}
             </div>
           </div>
-          <div className="col s6 m3">
-            <div className="input-field">
-              <label>
-                <input type="checkbox" className="filled-in" value="Type 2" defaultChecked={true} onChange={this.toggleConnectorTypes} />
-                <span>TYPE 2</span>
-              </label>
-            </div>
-          </div>
-          <div className="col s6 m3">
-            <div className="input-field">
-              <label>
-                <input type="checkbox" className="filled-in" value="CHAdeMO" defaultChecked={true} onChange={this.toggleConnectorTypes} />
-                <span>CHAdeMO</span>
-              </label>
-            </div>
-          </div>
-          <div className="col s6 m3">
-            <div className="input-field">
-              <label>
-                <input type="checkbox" className="filled-in" value="CCS" defaultChecked={true} onChange={this.toggleConnectorTypes} />
-                <span>CCS</span>
-              </label>
-            </div>
-          </div>
+
+          {this.connectorTypeOptions.map((ctype) => {
+            return (
+              <div className="col s6 m3">
+                <div className="input-field">
+                  <label>
+                    <input type="checkbox" className="filled-in" value={ctype} defaultChecked={connectorTypes.has(ctype)} onChange={this.toggleConnectorTypes} />
+                    <span>{ctype}</span>
+                  </label>
+                </div>
+              </div>
+            )
+          })}
+
         </div>
       </div>
     )
